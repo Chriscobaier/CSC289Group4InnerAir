eacd2e40e62317041ad04050fc9ff06a3ee3bd8d
Jrchavez09 <90872196+Jrchavez09@users.noreply.github.com>
2023-02-02 19:00:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 import sqlite3
 
-#todo import flask forms stuffs
+# todo import flask forms stuffs
 
 app = Flask(__name__)
 
@@ -22,5 +22,15 @@ def register():
     return render_template("register.html")
 
 
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    pass
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True
