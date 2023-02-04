acca6abbe12db3d4c3f9ee8b9aa98920374b1ee5
Chriscobaier <47674302+Chriscobaier@users.noreply.github.com>
2023-02-04 15:39:47 -0500

    Create test_db_models.py

---
+++ test_db_models.py
@@ -0,0 +1,52 @@
+import pytest
+from models import User, Exercise, Routine, Favorites, Statistics, Category, UserRating, db
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '`5J<-lgHQaae_|LR*h)0%}`#k?sW@IK],P-9,A/}d`Ly&GwruSUh#omM]AdXwNP'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+@pytest.fixture(scope="module")
+def db_session():
+    test_user = User(firstname="testUser", email="testUser@example.com", password="changeme")
+    test_exercise = Exercise(exercise_name='test_exercise_name',
+                             exercise_instructions='test_exercise_instructions',
+                             exercise_description='test_exercise_descriptions',
+                             exercise_length=12, cumulative_rating=100, category_id=1)
+    with app.app_context():
+        db.session.add(test_user)
+        db.session.add(test_exercise)
+        db.session.commit()
+        yield db.session
+        db.session.remove()
+
+
+def test_user_model(db_session):
+    test_user = db.session.query(User).first()
+    assert test_user.firstname == "testUser"
+    assert test_user.email == "testUser@example.com"
+    assert test_user.password == "changeme"
+
+
+def test_exercise_model(db_session):
+    test_exercise = db.session.query(Exercise).first()
+    assert test_exercise.exercise_name == 'test_exercise_name'
+    assert test_exercise.exercise_instructions == 'test_exercise_instructions'
+    assert test_exercise.exercise_description == 'test_exercise_descriptions'
+    assert test_exercise.exercise_length == 12
+    assert test_exercise.cumulative_rating == 100
+
+
+def test_routine_model(db_session):
+    test_routine_add = Routine(user_id=db.session.query(User).first().userID,
+                               exercise_id=db.session.query(Exercise).first().exerciseID)
+    db.session.add(test_routine_add)
+    db.session.commit()
+    test_routine = db.session.query(Routine).first()
+    assert test_routine.user_id == db.session.query(User).first().userID
+    assert test_routine.exercise_id == db.session.query(Exercise).first().exerciseID
