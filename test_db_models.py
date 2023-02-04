4904902c3aaeba7c3287e40030c51c29c390cb39
Chriscobaier <47674302+Chriscobaier@users.noreply.github.com>
2023-02-04 16:55:39 -0500

    Update test_db_models.py

--- test_db_models.py
+++ test_db_models.py
@@ -1,6 +1,7 @@
 import pytest
 from models import User, Exercise, Routine, Favorites, Statistics, Category, UserRating, db
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '`5J<-lgHQaae_|LR*h)0%}`#k?sW@IK],P-9,A/}d`Ly&GwruSUh#omM]AdXwNP'
@@ -31,6 +32,7 @@ def test_user_model(db_session):
     assert test_user.firstname == "testUser"
     assert test_user.email == "testUser@example.com"
     assert test_user.password == "changeme"
+    print(f"\n Dictionary of User Object: {test_user.as_dict()} \n")
 
 
 def test_exercise_model(db_session):
@@ -40,6 +42,7 @@ def test_exercise_model(db_session):
     assert test_exercise.exercise_description == 'test_exercise_descriptions'
     assert test_exercise.exercise_length == 12
     assert test_exercise.cumulative_rating == 100
+    print(f"\n Dictionary of exercise Object: {test_exercise.as_dict()} \n")
 
 
 def test_routine_model(db_session):
@@ -50,3 +53,16 @@ def test_routine_model(db_session):
     test_routine = db.session.query(Routine).first()
     assert test_routine.user_id == db.session.query(User).first().userID
     assert test_routine.exercise_id == db.session.query(Exercise).first().exerciseID
+    print(f"\n Dictionary of Routine Object: {test_routine.as_dict()} \n")
+
+
+def test_favorites_model(db_session):
+    test_favorites_add = Favorites(user_id=db.session.query(User).first().userID,
+                                   exercise_id=db.session.query(Exercise).first().exerciseID)
+    db.session.add(test_favorites_add)
+    db.session.commit()
+    assert test_favorites_add.user_id == db.session.query(User).first().userID
+    assert test_favorites_add.exercise_id == db.session.query(Exercise).first().exerciseID
+    print(f"\n Dictionary of Favorites Object: {test_favorites_add.as_dict()} \n")
+
+
