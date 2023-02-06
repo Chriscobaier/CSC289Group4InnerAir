75ad5f406a4f53655c148d56d95c018b7eccb8d9
Chriscobaier <47674302+Chriscobaier@users.noreply.github.com>
2023-02-06 16:01:17 -0500

    Adding json files to help build database, not used in app for now

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'User.Users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     firstname = db.Column(db.String(64), nullable=False)
