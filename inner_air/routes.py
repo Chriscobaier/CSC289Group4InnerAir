c90b980ee218ff42bedbc2e038ac205fb7be4221
Chriscobaier <47674302+Chriscobaier@users.noreply.github.com>
2023-02-07 10:22:11 -0500

    Please read comments, this will drop all tables, and create db with 10 test exercises

--- inner_air/routes.py
+++ inner_air/routes.py
@@ -6,6 +6,25 @@ from inner_air.forms import RegistrationForm, LoginForm
 from inner_air.models import Exercise, User, Routine, Favorites, Statistics, Category, UserRating, DBVersion
 
 
+# COMMENT THIS OUT IF YOU DON'T WANT TO DELETE YOUR DATABASE
+def DeleteAndCreateDB():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        for i in range(10):
+            db.session.add(Exercise(exercise_name='test_exercise_name' + f' {i}',
+                                    exercise_instructions='test_exercise_instructions' + f' {i}',
+                                    exercise_description='test_exercise_descriptions' + f' {i}',
+                                    exercise_length=12, category_id=1))
+            db.session.commit()
+
+
+# COMMENT THIS OUT IF YOU DON'T WANT TO DELETE YOUR DATABASE
+# COMMENT THIS OUT IF YOU DON'T WANT TO DELETE YOUR DATABASE
+# COMMENT THIS OUT IF YOU DON'T WANT TO DELETE YOUR DATABASE
+DeleteAndCreateDB()
+
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -44,7 +63,7 @@ def register():
         if db.session.query(User).filter_by(email=form.email.data).first() is None:
             db.session.add(user)
             db.session.commit()
-            login_user(user)
+            login_user(user, remember=True)
             flash(f'Account created successfully for {form.firstname.data}', category='success')
             return redirect(url_for('login'))
 
@@ -61,6 +80,27 @@ def dashboard():
     """
     users = User.query.all()
     return render_template('dashboard.html', users=users)
+
+
+@app.route('/exercises', methods=['GET', 'POST'])
+def exercises():
+    if request.method == 'POST':
+        if "favoriteButton" in request.form:
+            currentUser = request.form['user_id']
+            currentExercise = request.form['exercise_id']
+            if db.session.query(Favorites).filter(Favorites.user_id == currentUser).filter(
+                    Favorites.exercise_id == currentExercise).first() is None:
+                db.session.add(Favorites(user_id=currentUser, exercise_id=currentExercise))
+                db.session.commit()
+            else:
+                flash("This is already a favorite, TODO REMOVE BUTTON IF ALREADY FAVORITE", category='danger')
+        elif "routineButton" in request.form:
+            pass
+        else:
+            pass
+    exercise_list = Exercise.query.all()
+    favorite_list = Favorites.query.all()
+    return render_template('exercises.html', exercises=exercise_list, favorites=favorite_list)
 
 
 @app.route('/logout')
