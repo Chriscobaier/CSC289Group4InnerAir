ae590ef216a1eff50243efed75fc8808eb56bce3
Chriscobaier <47674302+Chriscobaier@users.noreply.github.com>
2023-02-11 10:11:42 -0500

    added db automation, filled some data

--- inner_air/routes.py
+++ inner_air/routes.py
@@ -1,3 +1,5 @@
+import json
+
 import flask_login
 
 from inner_air import app, db
@@ -6,25 +8,82 @@ from flask_login import login_user, logout_user, login_required
 
 from inner_air.forms import RegistrationForm, LoginForm
 from inner_air.models import Exercise, User, Routine, Favorites, Statistics, Category, UserRating, DBVersion
+from datetime import datetime, timedelta
 
 
 # COMMENT THIS OUT IF YOU DON'T WANT TO DELETE YOUR DATABASE
 def DeleteAndCreateDB():
     with app.app_context():
-        db.drop_all()
-        db.create_all()
-        for i in range(10):
-            db.session.add(Exercise(exercise_name='test_exercise_name' + f' {i}',
-                                    exercise_instructions='test_exercise_instructions' + f' {i}',
-                                    exercise_description='test_exercise_descriptions' + f' {i}',
-                                    exercise_length=12, category_id=1))
+        try:
+            thisVersion = db.session.query(DBVersion).order_by(DBVersion.version.desc()).first()
+        except:
+            thisVersion = None
+
+        if thisVersion is None:
+            db.drop_all()
+            db.create_all()
+            with open('importdata/data.json') as f:
+                data = json.load(f)
+                j = data['exercises']
+                for i in j:
+                    db.session.add(Exercise(exercise_name=i["name"],
+                                            exercise_instructions=i["exercise_instructions"],
+                                            exercise_description=i["exercise_description"],
+                                            exercise_length=i["exercise_length"], category_id=i["category_id"]))
+                    db.session.commit()
+                j = data['users']
+                for i in j:
+                    db.session.add(User(firstname=i["firstname"],
+                                        email=i["email"],
+                                        password_hash=i["password_hash"]))
+                for i in range(365):
+                    x = datetime.today() - timedelta(days=i)
+                    db.session.add(Statistics(date_completed=x, user_id=1, exercise_id=1))
+                    db.session.commit()
+                for i in range(45):
+                    x = datetime.today() - timedelta(days=i)
+                    db.session.add(Statistics(date_completed=x, user_id=2, exercise_id=1))
+                    db.session.commit()
+                for i in range(7):
+                    x = datetime.today() - timedelta(days=i)
+                    db.session.add(Statistics(date_completed=x, user_id=3, exercise_id=3))
+                    db.session.commit()
+            db.session.add(DBVersion(version='0.01'))
+            db.session.commit()
+        elif str(thisVersion.version) < '0.01':
+            db.drop_all()
+            db.create_all()
+            with open('importdata/data.json') as f:
+                data = json.load(f)
+                j = data['exercises']
+                for i in j:
+                    db.session.add(Exercise(exercise_name=i["name"],
+                                            exercise_instructions=i["exercise_instructions"],
+                                            exercise_description=i["exercise_description"],
+                                            exercise_length=i["exercise_length"], category_id=i["category_id"]))
+                    db.session.commit()
+                j = data['users']
+                for i in j:
+                    db.session.add(User(firstname=i["firstname"],
+                                        email=i["email"],
+                                        password_hash=i["password_hash"]))
+                for i in range(365):
+                    x = datetime.today() - timedelta(days=i)
+                    db.session.add(Statistics(date_completed=x, user_id=1, exercise_id=1))
+                    db.session.commit()
+                for i in range(45):
+                    x = datetime.today() - timedelta(days=i)
+                    db.session.add(Statistics(date_completed=x, user_id=2, exercise_id=1))
+                    db.session.commit()
+                for i in range(7):
+                    x = datetime.today() - timedelta(days=i)
+                    db.session.add(Statistics(date_completed=x, user_id=3, exercise_id=3))
+                    db.session.commit()
+            db.session.add(DBVersion(version='0.01'))
             db.session.commit()
 
 
-# COMMENT THIS OUT IF YOU DON'T WANT TO DELETE YOUR DATABASE
-# COMMENT THIS OUT IF YOU DON'T WANT TO DELETE YOUR DATABASE
-# COMMENT THIS OUT IF YOU DON'T WANT TO DELETE YOUR DATABASE
-# DeleteAndCreateDB()
+DeleteAndCreateDB()
 
 
 @app.route('/')
