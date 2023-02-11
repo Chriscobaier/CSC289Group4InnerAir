dd2a063153e88812fce23ded69d7f379373b86b1
Chriscobaier <47674302+Chriscobaier@users.noreply.github.com>
2023-02-11 14:30:35 -0500

    Added user days tracker

--- inner_air/routes.py
+++ inner_air/routes.py
@@ -1,11 +1,13 @@
 import flask_login
-
+import json
 from inner_air import app, db
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
 
 from inner_air.forms import RegistrationForm, LoginForm
 from inner_air.models import Exercise, User, Routine, Favorites, Statistics, Category, UserRating, DBVersion
+from datetime import datetime,timedelta
+
 
 
 def DeleteAndCreateDB():
@@ -90,6 +92,61 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    # Check if the request method is POST
+    if request.method == 'POST':
+        # Check if the request form contains "favoriteButton" key
+        if "favoriteButton" in request.form:
+            # Retrieve user and exercise IDs from the request form
+            currentUser = request.form['user_id']
+            currentExercise = request.form['exercise_id']
+            # Check if the current combination of user and exercise IDs is already in the Favorites table
+            if db.session.query(Favorites).filter(Favorites.user_id == currentUser).filter(
+                    Favorites.exercise_id == currentExercise).first() is None:
+                db.session.add(Favorites(user_id=currentUser, exercise_id=currentExercise))
+                db.session.commit()
+
+        # Check if the request form contains "favoriteButtonRemove" key
+        elif "favoriteButtonRemove" in request.form:
+            # Retrieve user and exercise IDs from the request for
+            currentUser = request.form['user_id']
+            currentExercise = request.form['exercise_id']
+            # Check if the current combination of user and exercise IDs is in the Favorites table
+            if db.session.query(Favorites).filter(Favorites.user_id == currentUser).filter(
+                    Favorites.exercise_id == currentExercise).first() is not None:
+                toDelete = db.session.query(Favorites).filter(Favorites.user_id == currentUser).filter(
+                    Favorites.exercise_id == currentExercise).first()
+                db.session.delete(toDelete)
+                db.session.commit()
+        else:
+            pass
+    # Retrieve all entries from the Exercise and Favorites tables
+    exercise_list = Exercise.query.all()
+    favorite_list = Favorites.query.all()
+
+    # Define a function to show the exercises that the current user has in the Favorites table
+    def showFav():
+        listOfExerciseCurrentUserHasInFavorites = []
+        for favorite in favorite_list:
+            if favorite.as_dict()['user_id'] == flask_login.current_user.id:
+                listOfExerciseCurrentUserHasInFavorites.append(favorite.as_dict()['exercise_id'])
+        return listOfExerciseCurrentUserHasInFavorites
+
+    xDataAll = db.session.query(Statistics).filter_by(user_id=flask_login.current_user.id).all()
+    xData = []
+    yData = []
+    j = 0
+    for i in xDataAll:
+        j += 1
+        xData.append(i.date_completed)
+        # Need to count # of exercises per day
+        yData.append(j)
+
+    return render_template('profile.html', exercises=exercise_list, favorites=favorite_list, showFavAdd=showFav(),
+                           xData=xData, yData=yData)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -99,8 +156,9 @@ def login():
 
         if user and user.verify_password(attempted_password=form.password.data):
             login_user(user)
+            user.updateLastLogin()
             flash(f'Success! You are logged in as: {user.firstname}', category='success')
-            return redirect(url_for('dashboard'))
+            return redirect(url_for('profile'))
         else:
             flash('You have entered an invalid email address or password.', category='danger')
     return render_template('login.html', form=form)
@@ -122,21 +180,21 @@ def register():
             db.session.commit()
             login_user(user)
             flash(f'Account created successfully for {form.firstname.data}', category='success')
-            return redirect(url_for('dashboard'))
+            return redirect(url_for('profile'))
 
         else:
             flash("This email already exists.  Try logging in, or register with a different email", category='danger')
     return render_template("register.html", form=form)
 
 
-@app.route('/dashboard', methods=['GET', 'POST'])
+@app.route('/userlist', methods=['GET', 'POST'])
 @login_required
-def dashboard():
+def userlist():
     """
         I tried to create a table so that something can be displayed, nothing fancy.
     """
     users = User.query.all()
-    return render_template('dashboard.html', users=users)
+    return render_template('userlist.html', users=users)
 
 
 @app.route('/exercises', methods=['GET', 'POST'])
