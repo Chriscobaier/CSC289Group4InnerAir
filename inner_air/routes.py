b679fe602c5e9c49380aa163c15bb651dcafd35f
Chriscobaier <47674302+Chriscobaier@users.noreply.github.com>
2023-02-11 11:26:35 -0500

    Added Profile html, user can add favorites, and see number of exercises completed

--- inner_air/routes.py
+++ inner_air/routes.py
@@ -92,6 +92,61 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    # Check if the request method is POST
+    if request.method == 'POST':
+        # Check if the request form contains "favoriteButton" key
+        if "favoriteButton" in request.form:
+            # Retrieve user and exercise IDs from the request form
+            currentUser = request.form['user_id']
+            currentExercise = request.form['exercise_id']
+            # Check if the current combination of user and exercise IDs is already in the Favorites table
+            if db.session.query(Favorites).filter(Favorites.user_id == currentUser).filter(
+                    Favorites.exercise_id == currentExercise).first() is None:
+                db.session.add(Favorites(user_id=currentUser, exercise_id=currentExercise))
+                db.session.commit()
+
+        # Check if the request form contains "favoriteButtonRemove" key
+        elif "favoriteButtonRemove" in request.form:
+            # Retrieve user and exercise IDs from the request for
+            currentUser = request.form['user_id']
+            currentExercise = request.form['exercise_id']
+            # Check if the current combination of user and exercise IDs is in the Favorites table
+            if db.session.query(Favorites).filter(Favorites.user_id == currentUser).filter(
+                    Favorites.exercise_id == currentExercise).first() is not None:
+                toDelete = db.session.query(Favorites).filter(Favorites.user_id == currentUser).filter(
+                    Favorites.exercise_id == currentExercise).first()
+                db.session.delete(toDelete)
+                db.session.commit()
+        else:
+            pass
+    # Retrieve all entries from the Exercise and Favorites tables
+    exercise_list = Exercise.query.all()
+    favorite_list = Favorites.query.all()
+
+    # Define a function to show the exercises that the current user has in the Favorites table
+    def showFav():
+        listOfExerciseCurrentUserHasInFavorites = []
+        for favorite in favorite_list:
+            if favorite.as_dict()['user_id'] == flask_login.current_user.id:
+                listOfExerciseCurrentUserHasInFavorites.append(favorite.as_dict()['exercise_id'])
+        return listOfExerciseCurrentUserHasInFavorites
+
+    xDataAll = db.session.query(Statistics).filter_by(user_id=flask_login.current_user.id).all()
+    xData = []
+    yData = []
+    j = 0
+    for i in xDataAll:
+        j += 1
+        xData.append(i.date_completed)
+        # Need to count # of exercises per day
+        yData.append(j)
+
+    return render_template('profile.html', exercises=exercise_list, favorites=favorite_list, showFavAdd=showFav(),
+                           xData=xData, yData=yData)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
