99c2f3f533faefc37dbd5deed8e7c022a37908be
Karina Osmanova <kmosmanova@my.waketech.edu>
2023-02-28 23:34:42 -0500

    Add animation for expanding card

---
+++ inner_air/exercises/static/js/expand_list_element.js
@@ -0,0 +1,16 @@
+let list_sections = document.getElementsByClassName("name");
+
+function expander(event) {
+    let exercise_card = event.target.closest(".plate").querySelector(".description");
+    
+    if (exercise_card.style.display == "none" || !exercise_card.style.display) {
+        exercise_card.style.display = "block";
+    } else {
+        exercise_card.style.display = "none";
+    }
+}
+
+for (let i = 0; i < list_sections.length; i++) {
+    console.log(list_sections[i])
+    list_sections[i].addEventListener('click', expander);
+  }
