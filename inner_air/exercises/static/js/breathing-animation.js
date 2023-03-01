5df240522571b454c0c6cc97db879dc58cd708f9
Karina Osmanova <kmosmanova@my.waketech.edu>
2023-02-28 20:24:43 -0500

    Restructure button class

--- inner_air/exercises/static/js/breathing-animation.js
+++ inner_air/exercises/static/js/breathing-animation.js
@@ -4,7 +4,7 @@ start_button.addEventListener("click", switch_button);
 //changes to stop/start state of the button
 function switch_button() {
     if (start_button.classList.contains("button--green")){
-        start_button.className = "button";
+        start_button.className = "button--red";
         start_button.textContent = "Stop"
     } else {
         start_button.className = "button--green";
