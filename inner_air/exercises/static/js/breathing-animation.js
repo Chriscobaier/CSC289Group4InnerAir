fc21d2af2c463bf5c4ac2fddda5ff571f8502300
Karina Osmanova <kmosmanova@my.waketech.edu>
2023-03-04 15:37:10 -0500

    Implement timer

--- inner_air/exercises/static/js/breathing-animation.js
+++ inner_air/exercises/static/js/breathing-animation.js
@@ -1,21 +1,4 @@
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-// button animation
-let start_button = document.getElementById("start-button");
-start_button.addEventListener("click", switch_button);
-
-// changes to stop/start state of the button
-function switch_button() {
-    if (start_button.classList.contains("button--green")) {
-        start_button.className = "button--red";
-        start_button.textContent = "Stop";
-        feather_cycle(cycle_count);
-    } else {
-        start_button.className = "button--green";
-        start_button.textContent = "Start";
-    }
-}
-
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // animation variables
 let inhale_time;
 let inhale_hold;
@@ -47,17 +30,59 @@ xhr.onerror = function () {
 xhr.send();
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// button animation
+let start_button = document.getElementById("start-button");
+start_button.addEventListener("click", switch_button);
+
+// changes to stop/start state of the button
+function switch_button() {
+    if (start_button.classList.contains("button--green")) {
+        start_button.className = "button--red";
+        start_button.textContent = "Stop";
+        feather_cycle(cycle_count);
+    } else {
+        start_button.className = "button--green";
+        start_button.textContent = "Start";
+    }
+}
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // animation logic
 let feather = document.getElementById("feather");
 let animate_scope = document.getElementById("scope");
+let timer = document.getElementById("timer");
 let position = 0;
 
+/* Changes contents of the timer */
+function timer_count(state, time, timeout) {
+    setTimeout(() => {
+        timer.textContent = state + " for " + time + " seconds";
+        time--;
+        let id = setInterval(() => {
+            timer.textContent = state + " for " + time + " seconds";
+            if (time == 1) {
+                clearInterval(id);
+            }
+            time--;
+        }, 1000);
+    }, timeout);
+}
+
+/* Sets timer cycle */
+function set_timer(ih_time, ih_hold, ex_time, ex_hold) {
+    timer_count("Inhale", ih_time);
+    timer_count("Inhale Hold", ih_time, ih_time * 1000);
+    timer_count("Exhale", ih_time, (ih_time + ih_hold) * 1000);
+    timer_count("Exhale Hold", ih_time, (ih_time + ih_hold + ex_time) * 1000);
+}
+
+/* Plays Inhale animation */
 function inhale() {
     feather.style.transitionDuration = inhale_time + "s";
     feather.className = "float-object bottom";
-    console.log("inhale");
 }
 
+/* Plays exhale animation with timing for inhale hold */
 function exhale() {
     setTimeout(() => {
         feather.style.transitionDuration = inhale_time + "s";
@@ -65,7 +90,9 @@ function exhale() {
     }, inhale_hold * 1000);
 }
 
+/* Loops the animation */
 function feather_cycle(cycle) {
+    set_timer(inhale_time, inhale_hold, exhale_time, exhale_hold);
     inhale();
     setTimeout(exhale, inhale_time * 1000);
 
@@ -74,7 +101,6 @@ function feather_cycle(cycle) {
         if (cycle == 0) {
             return;
         } else {
-            console.log(cycle);
             return feather_cycle(cycle - 1);
         }
     }, (inhale_time + inhale_hold + exhale_time + inhale_hold) * 1000);
