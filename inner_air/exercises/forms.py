ea993845dfbbbe1124ea39a72eb6e57ed182e2e1
Chriscobaier <47674302+Chriscobaier@users.noreply.github.com>
2023-03-25 17:04:15 -0400

    added word

--- inner_air/exercises/forms.py
+++ inner_air/exercises/forms.py
@@ -6,5 +6,5 @@ class RateEx(FlaskForm):
     RateField = RadioField(label='Rate this Exercise',
                            choices=[('0', '0'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')])
     submit = SubmitField(
-        label='Submit'
+        label='Submit Rating'
     )
