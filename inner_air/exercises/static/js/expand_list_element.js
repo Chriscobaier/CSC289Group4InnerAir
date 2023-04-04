1b1c3791c85a034f02faa69a135008cbf3c5b642
Karina Osmanova <kmosmanova@my.waketech.edu>
2023-04-04 19:56:17 -0400

    Expanded clickable area for exercises

--- inner_air/exercises/static/js/expand_list_element.js
+++ inner_air/exercises/static/js/expand_list_element.js
@@ -1,4 +1,5 @@
 let list_sections = document.getElementsByClassName("name");
+let list_plate = document.getElementsByClassName("plate");
 
 function expander(event) {
     let exercise_card = event.target.closest(".plate").querySelector(".description");
@@ -13,4 +14,5 @@ function expander(event) {
 for (let i = 0; i < list_sections.length; i++) {
     console.log(list_sections[i])
     list_sections[i].addEventListener('click', expander);
+    list_plate[i].addEventListener('click', expander);
   }
